fa80615908ac34fe314d0d984acc79c7650d7b44
wolfbyte <devon.dieffenbach@gmail.com>
2019-06-24 18:48:05 -0400

    Add project files.

---
+++ trachea/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace trachea
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.txtRoom = new System.Windows.Forms.TextBox();
+            this.txtRaw = new System.Windows.Forms.TextBox();
+            this.btnRaw = new System.Windows.Forms.Button();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.BackgroundWorker1_DoWork);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 110);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(767, 303);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(291, 22);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 23);
+            this.btnSend.TabIndex = 1;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.BtnSend2_Click);
+            // 
+            // txtRoom
+            // 
+            this.txtRoom.Location = new System.Drawing.Point(89, 25);
+            this.txtRoom.Name = "txtRoom";
+            this.txtRoom.Size = new System.Drawing.Size(174, 20);
+            this.txtRoom.TabIndex = 2;
+            this.txtRoom.Text = "#blackhat";
+            // 
+            // txtRaw
+            // 
+            this.txtRaw.Location = new System.Drawing.Point(89, 63);
+            this.txtRaw.Name = "txtRaw";
+            this.txtRaw.Size = new System.Drawing.Size(296, 20);
+            this.txtRaw.TabIndex = 3;
+            this.txtRaw.Text = "JOIN #chokey";
+            // 
+            // btnRaw
+            // 
+            this.btnRaw.Location = new System.Drawing.Point(391, 61);
+            this.btnRaw.Name = "btnRaw";
+            this.btnRaw.Size = new System.Drawing.Size(75, 23);
+            this.btnRaw.TabIndex = 4;
+            this.btnRaw.Text = "Send!";
+            this.btnRaw.UseVisualStyleBackColor = true;
+            this.btnRaw.Click += new System.EventHandler(this.BtnRaw_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(13, 418);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(687, 20);
+            this.txtMessage.TabIndex = 5;
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TxtMessage_KeyDown);
+            // 
+            // btnSend2
+            // 
+            this.btnSend2.Location = new System.Drawing.Point(707, 420);
+            this.btnSend2.Name = "btnSend2";
+            this.btnSend2.Size = new System.Drawing.Size(75, 23);
+            this.btnSend2.TabIndex = 6;
+            this.btnSend2.Text = "send";
+            this.btnSend2.UseVisualStyleBackColor = true;
+            this.btnSend2.Click += new System.EventHandler(this.BtnSend2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(23, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Channel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Raw Message";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(561, 25);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(68, 17);
+            this.checkBox1.TabIndex = 9;
+            this.checkBox1.Text = "Listening";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.CheckBox1_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSend2);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.btnRaw);
+            this.Controls.Add(this.txtRaw);
+            this.Controls.Add(this.txtRoom);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.TextBox txtRoom;
+        private System.Windows.Forms.TextBox txtRaw;
+        private System.Windows.Forms.Button btnRaw;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ trachea/Form1.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Windows.Forms;
+using System.IO;
+using System.Net.Sockets;
+using System.Security.Cryptography.X509Certificates;
+using System.Security.Authentication;
+using System.Net.Security;
+
+namespace trachea
+{
+    public partial class Form1 : Form
+    {
+        delegate void TextBoxDelegate(string text);
+        private TcpClient tcpClient;
+        private SslStream ns;
+        private StreamReader reader;
+        private StreamWriter writer;
+        private bool Listening;
+// private static Action<string> OutputLog;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void OutputLog(string text)
+        {
+            if (!richTextBox1.InvokeRequired)
+            {
+                richTextBox1.SelectionStart = richTextBox1.TextLength;
+                richTextBox1.SelectionLength = 0;
+                richTextBox1.SelectionColor = Color.DarkBlue;
+                richTextBox1.AppendText(text + "\r\n");
+                richTextBox1.SelectionColor = Color.Black;
+            }
+            else
+            {
+                TextBoxDelegate d = OutputLog;
+                this.Invoke(d, new object[] { text });
+            }
+        }
+
+        public void InputLog(string text)
+        {
+            if (!richTextBox1.InvokeRequired)
+            {
+                richTextBox1.SelectionStart = richTextBox1.TextLength;
+                richTextBox1.SelectionLength = 0;
+                richTextBox1.SelectionColor = Color.DarkRed;
+                richTextBox1.AppendText(text + "\r\n");
+                richTextBox1.SelectionColor = Color.Black;
+            }
+            else
+            {
+                TextBoxDelegate d = InputLog;
+                this.Invoke(d, new object[] { text });
+            }
+        }
+        private void Write(string line)
+        {
+            try
+            {
+                writer.WriteLine(line);
+                writer.Flush();
+                OutputLog(line);
+            }
+            catch (System.IO.IOException)
+            {
+
+            }
+
+        }
+        private void BridgeMain()
+        {
+            var input = string.Empty;
+            try
+            {
+                while ((input = reader.ReadLine()) != null)
+                {
+                    if(Listening) InputLog(input);
+                    var tokens = input.Split(' ');
+                    if (tokens[0].ToUpper() == "PING")
+                    {
+                        Write("PONG " + tokens[1]);
+                    }
+                    else
+                        switch (tokens[1].ToUpper())
+                        {
+                            case "PRIVMSG":
+                                if (tokens[2].ToLower() == "zerocool" && tokens[3].ToLower().Contains("version"))
+                                {
+                                    //Write("PRIVMSG #TOP wazzup");
+                                    string nick = tokens[0].Split('!')[0].Split(':')[1];
+                                    Write($"NOTICE {nick} :VERSION mIRC v7.43");
+                                }
+                                if(tokens[2].ToLower() == "#blackhat")  InputLog(input); 
+                                break;
+                            case "001":
+                                Write("JOIN #blackhat"); // 
+                                //Write("JOIN #chokey");
+                                break;
+                            default:
+                                break;
+                        }
+                }
+            }
+            catch (Exception err)
+            {
+
+            }
+        }
+        public void StartBridge() {
+            CheckConnection();
+            Write($"USER ZeroCool * * :Is that all you got?");
+            Write("NICK ZeroCool");
+            BridgeMain();
+        }
+        private void BackgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            StartBridge();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listening = true ;
+            OutputLog("Starting Timer");
+            backgroundWorker1.RunWorkerAsync();
+            timer1.Interval = 9000;
+            timer1.Start();
+        }
+        public static bool ValidateServerCertificate(object sender, X509Certificate certificate, X509Chain chain,
+        SslPolicyErrors sslPolicyErrors)
+        {
+            return true;
+        }
+        public void CheckConnection()
+        {
+            if (tcpClient == null)
+            {
+                tcpClient = new TcpClient("167.114.221.137", 6697);
+                ns = new SslStream(tcpClient.GetStream(),false, new RemoteCertificateValidationCallback (ValidateServerCertificate));
+                //ns = tcpClient?.GetStream();
+                ns.AuthenticateAsClient("breaking");
+                reader = new StreamReader(ns);
+                writer = new StreamWriter(ns);
+            }
+        }
+
+        private void BtnSend2_Click(object sender, EventArgs e)
+        {
+            Write($"PRIVMSG {txtRoom.Text} {txtMessage.Text}");
+        }
+
+        private void TxtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Enter)
+            {
+                Write($"PRIVMSG {txtRoom.Text} {txtMessage.Text}");
+                txtMessage.Clear();
+            }
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            Listening = false;
+            OutputLog("Stopping Timer");
+        }
+
+        private void BtnRaw_Click(object sender, EventArgs e)
+        {
+            Write(txtRaw.Text);
+        }
+
+        private void CheckBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) Listening = true;
+            else Listening = false;
+
+        }
+    }
+}
